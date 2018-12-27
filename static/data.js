208fe385bd97d02c627799264f55bbbd016f77e7
Khushal Patel <patelkhushal@ibm.com@Khushals-MacBook-Pro.local>
2018-12-27 00:49:07 -0500

    added new buttons and enhanced older functionalities

--- static/data.js
+++ static/data.js
@@ -5,7 +5,6 @@ var colAddCounter = 0; //track number of columns user added during a session. Th
 var table = new Tabulator("#example-table", {
     data: jsondata,
     layout: "fitColumns",
-    selectable: true,
     history: true,
     columns: coldata,
     //[{"editableTitle": true, "field": "id", "title": "id"}, {"editableTitle": true, "editor": "input", "field": "name", "title": "Name"}, {"editableTitle": true, "editor": "input", "field": "age", "title": "Age"}, {"editableTitle": true, "editor": "input", "field": "col", "title": "Fav Color"}, {"editableTitle": true, "editor": "input", "field": "dob", "title": "DOB"}],
@@ -14,10 +13,17 @@ var table = new Tabulator("#example-table", {
         currColComp = cell.getColumn();
         currRowComp = cell.getRow();
     },
-    columnTitleChanged:function(column){
+    columnTitleChanged: function (column) {
         //column - column component
         column.getDefinition()["field"] = column.getDefinition()["title"]; // set field value to the new title
-        },
+    },
+    rowClick: function (e, row) {
+        //e - the click event object
+        //row - row component
+        if ($('#selectable').is(":checked")) { //only allow row select if row select checkbox is checked
+            row.toggleSelect(); //toggle row selected state on row click
+        }
+    },
 });
 
 //submit button
@@ -38,6 +44,15 @@ function ajaxPostToFlask(flaskUrl, dataToPost) {
     });
 }
 
+//fix ids of the row. In case of addition or deletion of a row
+function formatIds() {
+    var tableData = table.getData();
+    for (i = 0; i < tableData.length; i++) {
+        tableData[i]["id"] = (i + 1);
+    }
+    table.setData(tableData);
+}
+
 //reset button
 $("#reset-data").on("click", function () {
     window.location.reload();
@@ -45,7 +60,10 @@ $("#reset-data").on("click", function () {
 
 //select all button
 $("#select-all").on("click", function () {
-    table.selectRow();
+    if ($('#selectable').is(":checked")) { //only allow row selection if row select checkbox is checked
+        table.selectRow();
+    }
+    
 });
 
 //Deselect all button
@@ -63,22 +81,30 @@ $("#history-redo").on("click", function () {
     table.redo();
 });
 
+//add row button
+$("#add-row").on("click", function () {
+    addRow(true, 0); //true adds row above the selected row
+});
+
 //add row above button
 $("#add-row-above").on("click", function () {
-    addRow(true); //true adds row above the selected row
+    if (currRowComp){
+        addRow(true, currRowComp.getIndex()); //true adds row above the selected row
+    }
 });
 
 //add row below button
 $("#add-row-below").on("click", function () {
-    addRow(false); //false adds row below the selected row
-
+    if (currRowComp){
+        addRow(false, currRowComp.getIndex()); //false adds row below the selected row
+    }
+    
 });
 
-function addRow(above) {
+function addRow(above, id) {
     var rowdata = table.getSelectedData();
-    if (rowdata.length == 0) { // if user has no row selected
-        console.log(currRowComp.getIndex());
-        table.addRow({ id: "" }, above, currRowComp.getIndex());
+    if (rowdata.length == 0 ) { // if user has no row selected
+        table.addRow({ id: "" }, above, id);
     }
     else {
         rowid = rowdata[rowdata.length - 1]["id"];
@@ -87,10 +113,10 @@ function addRow(above) {
     }
 }
 
-//delete row below button
+//delete row(s) button
 $("#delete-row").on("click", function () {
     var rowdata = table.getSelectedData();
-    if (rowdata.length == 0) {
+    if (rowdata.length == 0 && currRowComp) {
         table.deleteRow(currRowComp);
     }
     else {
@@ -112,11 +138,11 @@ $("#add-column-right").on("click", function () {
     addColumn(false); // false adds column to the right of the current column
 });
 
-function addColumn(left){
+function addColumn(left) {
     if (currColComp) {
         var colName = "new_column_" + (colAddCounter + 1).toString();
         console.log(colName);
-        table.addColumn({ "editableTitle": true, "editor": "input", "field": colName, "title": colName}, left, currColComp);
+        table.addColumn({ "editableTitle": true, "editor": "input", "field": colName, "title": colName }, left, currColComp);
         colAddCounter++;
     }
 }
@@ -128,11 +154,8 @@ $("#delete-column").on("click", function () {
     }
 });
 
-//fix ids of the row. In case of addition or deletion of a row
-function formatIds() {
-    var tableData = table.getData();
-    for (i = 0; i < tableData.length; i++) {
-        tableData[i]["id"] = (i + 1);
-    }
-    table.setData(tableData);
-}
+$('#selectable').change(function() {
+    if(!$(this).is(":checked")) {
+        table.deselectRow();   
+    }        
+});
